ea4b44df8da39b573161a15ece5b1154222f351b
Piotr Mazur <piotr_mazur@hotmail.com>
2026-05-30 18:35:50 +0200

    unify *Recipes.java for non-tiered items

--- src/main/java/pepin/pepeforge/tools/chisel/ChiselRecipes.java
+++ src/main/java/pepin/pepeforge/tools/chisel/ChiselRecipes.java
@@ -1,11 +1,17 @@
 package pepin.pepeforge.tools.chisel;
 
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 import pepin.pepeforge.item.ItemFactory;
 import pepin.pepeforge.recipe.RecipeRegistrar;
 
 public final class ChiselRecipes {
+
+    private static final Material TIP_MATERIAL = Material.STONE;
+    private static final Material CORE_MATERIAL = Material.IRON_INGOT;
+    private static final Material HANDLE_MATERIAL = Material.STICK;
 
     private final JavaPlugin plugin;
     private final ItemFactory itemFactory;
@@ -16,23 +22,26 @@ public final class ChiselRecipes {
     }
 
     public void registerAll() {
+
+        NamespacedKey key = ChiselRecipeKeys.CHISEL;
+
+        RecipeRegistrar.remove(plugin, key);
+        
         if (!itemFactory.isRecipeEnabled(ChiselDefinition.ITEM_ID)) {
             return;
         }
 
-        RecipeRegistrar.remove(plugin, ChiselRecipeKeys.CHISEL);
-
-        ShapedRecipe recipe = new ShapedRecipe(ChiselRecipeKeys.CHISEL, itemFactory.createChisel());
+        ShapedRecipe recipe = new ShapedRecipe(key, itemFactory.createChisel());
         /*
          * [ ][I][ ]
          * [ ][C][ ]
          * [ ][S][ ]
          */
         recipe.shape(" I ", " C ", " S ");
-        recipe.setIngredient('I', ChiselDefinition.TOP_MATERIAL);
-        recipe.setIngredient('C', ChiselDefinition.CORE_MATERIAL);
-        recipe.setIngredient('S', ChiselDefinition.HANDLE_MATERIAL);
-        RecipeRegistrar.add(plugin, ChiselRecipeKeys.CHISEL, recipe);
+        recipe.setIngredient('I', TIP_MATERIAL);
+        recipe.setIngredient('C', CORE_MATERIAL);
+        recipe.setIngredient('S', HANDLE_MATERIAL);
+        RecipeRegistrar.add(plugin, key, recipe);
     }
 
     public void unregisterAll() {
